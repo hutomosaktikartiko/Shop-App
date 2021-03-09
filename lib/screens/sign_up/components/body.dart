42b5b4ac28752a82d5ad430dc0608a8daba8a586
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-03-09 14:35:27 +0700

    Sign Up Page

--- lib/screens/sign_up/components/body.dart
+++ lib/screens/sign_up/components/body.dart
@@ -1,8 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/components/social_card.dart';
+import 'package:shop_app/constans.dart';
+import 'package:shop_app/screens/sign_up/components/sign_up_form.dart';
+import 'package:shop_app/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.02,
+              ),
+              Text(
+                "Register Account",
+                style: headingStyle,
+              ),
+              Text(
+                "Complete your details or continue \nwith social media",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.03,
+              ),
+              SignUpForm(),
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.05,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialCard(
+                    icon: "assets/icons/google-icon.svg",
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: "assets/icons/facebook-2.svg",
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: "assets/icons/twitter.svg",
+                    press: () {},
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: getProportionateScreenHeight(15),
+              ),
+              Text(
+                "By continuing your confirm that you agree \nwith our Term and Condition",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: getProportionateScreenHeight(15),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/sign_up/components/sign_up_form.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components/custom_surffix_icon.dart';
+import 'package:shop_app/components/default_button.dart';
+import 'package:shop_app/components/form_error.dart';
+import 'package:shop_app/constans.dart';
+import 'package:shop_app/size_config.dart';
+
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  String email;
+  String password;
+  String confirmPassword;
+  final List<String> errors = [];
+
+  void addError({String error}) {
+    if (!errors.contains(error)) {
+      setState(() {
+        errors.add(error);
+      });
+    }
+  }
+
+  void removeError({String error}) {
+    if (error.contains(error)) {
+      setState(() {
+        errors.remove(error);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildEmailFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(25),
+          ),
+          buildPasswordFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(25),
+          ),
+          buildConfirmPasswordFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(25),
+          ),
+          FormError(errors: errors),
+          SizedBox(
+            height: getProportionateScreenHeight(10),
+          ),
+          Defaultbutton(
+            text: "Continue",
+            press: () {
+              if (_formKey.currentState.validate()) {}
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kPassNullError);
+        } else if (value.length >= 8) {
+          removeError(error: kShortPassError);
+        }
+        password = value;
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: kPassNullError);
+
+          return "";
+        } else if (value.length < 8) {
+          addError(error: kShortPassError);
+
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          hintText: "Enter your Password",
+          labelText: "Password",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          suffixIcon: CustomSurffixIcon(
+            svgIcon: "assets/icons/Lock.svg",
+          )),
+    );
+  }
+
+  TextFormField buildConfirmPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => confirmPassword = newValue,
+      onChanged: (value) {
+        if (password == confirmPassword) {
+          removeError(error: kMatchPassError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          return "";
+        } else if (password != confirmPassword) {
+          addError(error: kMatchPassError);
+
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          hintText: "Re-enter your Password",
+          labelText: "Confirm Password",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          suffixIcon: CustomSurffixIcon(
+            svgIcon: "assets/icons/Lock.svg",
+          )),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.remove(kEmailNullError);
+          });
+        } else if (emailValidatorRegExp.hasMatch(value) &&
+            errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.remove(kInvalidEmailError);
+          });
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.add(kEmailNullError);
+          });
+          return "";
+        } else if (!emailValidatorRegExp.hasMatch(value) &&
+            !errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.add(kInvalidEmailError);
+          });
+
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          hintText: "Enter your email",
+          labelText: "Email",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          suffixIcon: CustomSurffixIcon(
+            svgIcon: "assets/icons/Mail.svg",
+          )),
+    );
+  }
+}
