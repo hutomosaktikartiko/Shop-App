35d6bb46d32f83d7190c67d8e8988d39d3fa727f
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-03-17 23:24:21 +0700

    Home Screen Header

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/size_config.dart';
+import 'package:shop_app/screens/home/components/home_header.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: getProportionateScreenWidth(20),
+            ),
+            HomeHeader()
+          ],
+        ),
+      ),
+    );
   }
 }
