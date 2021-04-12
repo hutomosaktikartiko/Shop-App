196a99521dff33cbfed7c3f8a68e1bf5c86c79a9
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-04-12 21:57:58 +0700

    Cart List & Dismissible

--- lib/screens/home/components/home_header.dart
+++ lib/screens/home/components/home_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/cart/cart_screen.dart';
 import 'package:shop_app/screens/home/components/search_field.dart';
 import 'package:shop_app/screens/home/components/icon_btn_with_counter.dart';
 import 'package:shop_app/size_config.dart';
@@ -20,7 +21,7 @@ class HomeHeader extends StatelessWidget {
           IconBtnWithCounter(
             svgSrc: "assets/icons/Cart Icon.svg",
             numOfItems: 0,
-            press: () {},
+            press: () => Navigator.pushNamed(context, CartScreen.routeName),
           ),
           IconBtnWithCounter(
             svgSrc: "assets/icons/Bell.svg",
